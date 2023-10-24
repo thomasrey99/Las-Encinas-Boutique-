cf91a3977e834ae1ab11e9a18eefcaf9b7f0d578
NahimMora <nahimsalta@gmail.com>
2023-10-24 18:08:50 -0300

    Cards y Card con un array para probar funcionalidad (Falta style y función para recibir los productos del back), Form para crear productos falta funciones solamente para obtener las categorías y postear productos y servidor se levanta correctamente

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import Cards from './components/cards/Cards';
+import FormProduct from './views/FormProduct/FormProducts'
 
 import './App.css'
 
@@ -5,6 +7,8 @@ function App() {
   return (
     <main>
       <h1>Las Encinas Boutique</h1>
+      <FormProduct/>
+      <Cards/>
     </main>
   );
 }
--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,4 +1,14 @@
+import React from 'react'
 
-const Card = () => {};
+const Card = ({name, price}) => {
+  return (
+    <div>
+        <h1>Carta de {name}</h1>
+        <h2>{name}</h2>
+        <h2>${price}</h2>
+    </div>
 
-export default Card;
+  )
+}
+
+export default Card

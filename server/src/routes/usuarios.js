5f0726719f9bd42c18964c647376e5d7c9886c16
Mariano Masondo <nanomason@gmail.com>
2023-10-24 11:04:51 -0300

    Server raised 3001

--- server/src/routes/usuarios.js
+++ server/src/routes/usuarios.js
@@ -1,7 +1,10 @@
 const { Router } = require("express")
+const { getUsuarioId } = require("../handlers/usuariosHandlers")
+const postNewUser = require("../handlers/postUserHandler")
 
 const router = Router()
 
-const { getUsuarioId } = require("../handlers/usuariosHandlers")
-
 router.get("/", getUsuarioId)
+router.post("/", postNewUser);
+
+module.exports = router;

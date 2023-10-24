ffc2726dba8e0130dae4836ed1f1b87dd03d0009
ChristianPuyo <christianpuyotorres@gmail.com>
2023-10-24 09:35:53 -0500

    sincronizando la base de datos

--- server/index.js
+++ server/index.js
@@ -1,18 +1,15 @@
 const server=require("./src/app")
 require("dotenv").config()
+const { conn } = require('./src/db.js');
 
 const {PORT}=process.env
 
 
-//!inicializando el servidor
-
-// server.listen(PORT, ()=>{
-//     console.log(`Servidor de las Encinas boutique escuchando en el puerto ${PORT}!💥`)
-// })
+//!inicializando el servidor y sincronizando la base de datos
 
 conn.sync({ alter: true }).then(() => {
     server.listen(PORT, ()=>{
         console.log(`Servidor de las Encinas boutique escuchando en el puerto ${PORT}!💥`)
     })
-    }).catch(error => console.log(error.message)) 
-     
+}).catch(error => console.log(error.message)) 
+ 

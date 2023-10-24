58c0ec1adea8197cc1695c7471b0551b30a992ac
Mariano Masondo <116052945+MarianoMasondo@users.noreply.github.com>
2023-10-24 01:11:47 -0300

    First Commit (#26)

---
+++ server/src/models/Users.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define(
+        "User",{
+            id: {
+                type: DataTypes.UUID,
+                defaultValue: Datatypes.UUIDV4,
+                primaryKey: true,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            lastName: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {                
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            }
+
+        }
+     
+    )
+}

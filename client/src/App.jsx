324085567ca8e9c1a73190963a8365cd65a04871
Javier.pjs <javier.sosa.1911@gmail.com>
2023-10-24 21:35:50 -0300

    primeras rutas App y configuracion browserRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import Home from './VIEWS/Home/home';
+import Landing from './VIEWS/Landind/landig';
+import Detail from './VIEWS/Detail/detail';
 
-function App() {
+const App = () => {
 
   return (
     <main>
-      <h1>hola mundo!!</h1>
+      <Routes> 
+        <Route path='/' element={<Landing/>} />
+        <Route path='home' element={<Home/>} />
+        <Route path='detail/:id' element={<Detail/>} />        
+      </Routes>
     </main>
   )
 }

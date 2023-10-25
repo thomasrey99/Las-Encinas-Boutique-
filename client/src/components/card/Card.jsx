e54adc97054db2aa170e29cad2f8af83ec3e8572
Mariano Masondo <nanomason@gmail.com>
2023-10-25 18:52:29 -0300

    mariano

--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,14 +1,4 @@
-// import React from 'react'
 
-// const Card = ({name, price}) => {
-//   return (
-//     <div>
-//         <h1>Carta de {name}</h1>
-//         <h2>{name}</h2>
-//         <h2>${price}</h2>
-//     </div>
+const Card = () => {};
 
-//   )
-// }
-
-// export default Card
+export default Card;

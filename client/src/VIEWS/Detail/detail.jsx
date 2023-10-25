a1a5b4a12e9be98eceb3646d2cad8437011eb939
mateoleont <mateolt505@gmail.com>
2023-10-25 10:23:48 -0500

    detail

--- client/src/VIEWS/Detail/detail.jsx
+++ client/src/VIEWS/Detail/detail.jsx
@@ -1,22 +1,23 @@
-//import { useParams } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 const Detail = () => {
 
-    //const dispatch = useDispatch();
-    //const { id } = useParams();
+    const dispatch = useDispatch();
+    const { id } = useParams();
     //const detailProduct = useSelector(state => state);
+
+    useEffect(() => {
+        dispatch((id));
+    },[dispatch]);
+
     const detailProduct = { 
         name: "Chocolate Blanco", 
         id: 1, 
         precio: 2.99, 
         imagen: 'https://www.eltiempo.com/uploads/2023/03/13/640fb61bb084a.jpeg'
     };
-
-    // useEffect(() => {
-    //     dispatch();
-    // },[dispatch]);
 
     return(
         <div>

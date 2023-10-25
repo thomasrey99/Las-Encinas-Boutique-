8cd5407564286ad6af1e57805c4e4fc35945e338
mateoleont <mateolt505@gmail.com>
2023-10-24 19:54:12 -0500

    detail

--- client/src/VIEWS/Detail/detail.jsx
+++ client/src/VIEWS/Detail/detail.jsx
@@ -1,3 +1,22 @@
-const Detail = () => {};
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
+const Detail = () => {
+
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    //const detailProduct = useSelector(state => state);
+
+    useEffect(() => {
+        dispatch((id));
+    },[dispatch]);
+
+    return(
+        <div>
+            Detail
+        </div>
+    );
+};
 
 export default Detail;

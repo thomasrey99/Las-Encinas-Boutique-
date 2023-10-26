6b4027c89a0373ac966f86e22540a7353eb53b97
JavierSosaAdm <91162471+JavierSosaAdm@users.noreply.github.com>
2023-10-26 19:51:18 -0300

    main (#58)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,6 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './libs/redux/store.js';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './libs/redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
@@ -21,11 +14,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
-  <Provider store={store}>
-    <BrowserRouter>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </BrowserRouter>
-  </Provider>,
-)
+  )
+  

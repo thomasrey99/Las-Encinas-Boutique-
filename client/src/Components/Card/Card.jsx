7adb36bd166dcbef6b7b89b5366a0a11ab660dce
Mateo Leon <118778750+MateoLeon505@users.noreply.github.com>
2023-10-26 14:55:03 -0500

    Hice merge y agregué home y detail (#57)
    * vite
    
    * sin cambios importantes
    
    * Nahim (#21)
    
    * Agregar rutas: productos por name y id, usuario por id y todos los productos
    
    * Errores de importacion solcionados
    
    * Lily (#25)
    
    * change
    
    * Models diagram
    
    * First Commit (#26)
    
    * Mariano (#28)
    
    * First Commit
    
    * Server raised 3001
    
    * GitIgnore ok
    
    * modelos de categorias y productos
    
    * cree las query para crear y traer productos por nombre, configure el store
    
    * sin cambios importantes
    
    * Agrego Query getAllProducts
    
    * Agrego Query getAllProducts
    
    * agregue reducer de productos
    
    * agregue reducer de usuarios
    
    * detail
    
    * ch
    
    * sin cambios importantes
    
    * sin cambios importantes
    
    * Nahim (#21)
    
    * Agregar rutas: productos por name y id, usuario por id y todos los productos
    
    * Errores de importacion solcionados
    
    * Lily (#25)
    
    * change
    
    * Models diagram
    
    * First Commit (#26)
    
    * Mariano (#28)
    
    * First Commit
    
    * Server raised 3001
    
    * GitIgnore ok
    
    * modelos de categorias y productos
    
    * sin cambios importantes
    
    * Agrego Query getAllProducts
    
    * Agrego Query getAllProducts
    
    * agregue reducer de productos
    
    * vite
    
    * Nahim (#21)
    
    * Agregar rutas: productos por name y id, usuario por id y todos los productos
    
    * Errores de importacion solcionados
    
    * Lily (#25)
    
    * change
    
    * Models diagram
    
    * First Commit (#26)
    
    * Mariano (#28)
    
    * First Commit
    
    * Server raised 3001
    
    * GitIgnore ok
    
    * modelos de categorias y productos
    
    * sin cambios importantes
    
    * Agrego Query getAllProducts
    
    * Agrego Query getAllProducts
    
    * agregue reducer de usuarios
    
    * detail
    
    * Cambios pequeños
    
    * cambie la configuracion de la db, los modelos y las relaciones
    
    * modifique las rutas
    
    * funcionales las rutas de productos
    
    * cambios
    
    * nuevos cambios
    
    * correcion
    
    * agregue ruta para traer todos los usuarios, modifique y arregle rutas para traer usuarios y productos por id, y para traer todos los productos y por nombre
    
    * cambios
    
    * cambio
    
    * deje funcional las actions de productos
    
    * changes
    
    * sin cambios importantes
    
    * changes
    
    * loading de detail
    
    * Update Home.jsx
    
    * Update home.jsx
    
    * Home
    
    * Update Home.jsx
    
    * change
    
    * changes
    
    * card
    
    ---------
    
    Co-authored-by: thomasrey99 <thomas_rey1999@outlook.com>
    Co-authored-by: Nahim Mora <nahimsalta@gmail.com>
    Co-authored-by: LILYCERON <128845224+LILYCERON@users.noreply.github.com>
    Co-authored-by: Mariano Masondo <116052945+MarianoMasondo@users.noreply.github.com>

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,12 +1,4 @@
-const Card = ({name, price}) => {
-  return (
-    <div>
-        <h1>Carta de {name}</h1>
-        <h2>{name}</h2>
-        <h2>${price}</h2>
-    </div>
 
-  )
-}
+const Card = () => {};
 
 export default Card;
--- client/src/VIEWS/Home/Home.jsx
+++ client/src/VIEWS/Home/Home.jsx
@@ -1,9 +1,47 @@
-import Cards from "../../Components/Cards/Cards";
+// import { Cards } from '../../Components/cards/Cards.jsx';
+import { useGetAllProductsQuery } from '../../libs/redux/services/productsApi.js';
+import { Spin, Alert, Carousel } from 'antd';
+import styles from './home.module.css'
 
-const Home = () => {
+const Home = () =>{
+
+    const { data: products, isError, isLoading } = useGetAllProductsQuery();
+    console.log(products);
+
     return(
-        <Cards/>
-    )
-};
+        <div className ={styles.homeContainer}>
+            {/* { isLoading&&       
+                <Spin tip="Cargando" size="large">
+                    <div className="content" />
+                </Spin> 
+            }
+            { isError&&     
+                <Alert
+                message="Error"
+                description="Por favor, intente de nuevo más tarde."
+                type="error"
+                showIcon
+                />
+            } */}
+            <div className={styles.carouselContainer}>
+                <Carousel autoplay dots= {true}>
+                    <div className={styles.sliceContainer}>
+                        <img src="https://lalicorera.com/img/banners/Banner-chocolates.jpg" alt="imagen1" className={styles.images}/>
+                    </div>                    
+                    <div className={styles.sliceContainer}>
+                        <img src="https://images.ecestaticos.com/l5CHYyiQo9cvHpB-f26l6GJAWa4=/147x52:1960x1413/1200x900/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2Ff62%2F2d0%2F7af%2Ff622d07af6329223b76f0bf4dacfc760.jpg" alt="imagen1" className={styles.images}/>
+                    </div>
+                    <div className={styles.sliceContainer}>
+                        <img src="https://www.eltiempo.com/uploads/2023/03/13/640fb61bb084a.jpeg" alt="imagen1" className={styles.images}/>
+                    </div>
+                    <div className={styles.sliceContainer}>
+                        <img src="https://statics-cuidateplus.marca.com/cms/styles/natural/azblob/chocolate-tableta.jpg.webp?itok=ctVzStkD" alt="imagen1" className={styles.images}/>
+                    </div>
+                </Carousel>
+            </div>
+            {/* <Cards products = {products}/> */}
+        </div>
+    );
+}
 
 export default Home;

4d2c5ee81ca2fa861b3900b87b0afe1faa55ed97
thomasrey99 <thomas_rey1999@outlook.com>
2023-10-26 01:18:50 -0300

    deje funcional las actions de productos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import './App.css'
-import {useSelector} from "react-redux"
-
+import {useSelector, useDispatch} from "react-redux"
+import { useGetAllProductsQuery } from './libs/redux/services/productsApi'
+import { useEffect } from 'react'
+import { addProducts } from './libs/redux/features/productsSlice'
 function App() {
-  const products=useSelector((state)=>state.items.products)
-  console.log(products)  
+  
+  const dispatch=useDispatch()
 
+  const products=useSelector((state)=>state.items.allProducts)
+  
+  const {data}=useGetAllProductsQuery()
 
+  useEffect(()=>{
+    dispatch(addProducts(data))
+  },[])
+
+  console.log("productos en el estado",products)
   return (
     <main>
       <h1>hola mundo!!</h1>
--- client/src/components/card/Card.jsx
+++ client/src/components/card/Card.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Card = ({name, price}) => {
   return (
     <div>

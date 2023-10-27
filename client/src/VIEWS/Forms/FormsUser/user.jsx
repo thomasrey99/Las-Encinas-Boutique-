6c2a3e34c3a2f4f76d0d58b3aa6e2ca62dca6d59
NahimMora <nahimsalta@gmail.com>
2023-10-27 04:12:31 -0300

    Cards terminadas, todo ok para develop

--- client/src/VIEWS/Forms/FormsUser/user.jsx
+++ client/src/VIEWS/Forms/FormsUser/user.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
-const user = () => {
+const FormUser = () => {
   return (
-    <div>user</div>
+      <div>
+          <h1>Registro de Usuario</h1>
+          <form>
+              <label htmlFor="name">Nombre: </label>
+              <input type="text" />
+              <hr />
+              <label htmlFor="last_name">Apellido: </label>
+              <input type="text" />
+              <hr />
+              <label htmlFor="address">Dirección: </label>
+              <input type="text" />
+              <hr />
+              <label htmlFor="email">E-Mail: </label>
+              <input type="text" />
+              <hr />
+              <label htmlFor="phone">Teléfono: </label>
+              <input type="text" />
+              <hr />
+          </form>
+      </div>
   )
-}
+};
 
-export default user
+export default FormUser;

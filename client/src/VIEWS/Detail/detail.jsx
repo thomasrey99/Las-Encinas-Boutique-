bc4515c5a307a1c8aaf21f6e4a5d4ed1e93f359c
mateoleont <mateolt505@gmail.com>
2023-10-27 13:12:13 -0500

    cambios

--- client/src/VIEWS/Detail/detail.jsx
+++ client/src/VIEWS/Detail/detail.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useGetProductByIdQuery } from '../../libs/redux/services/productsApi';
-import { Spin, Space, Alert, Card, Col, Row, Rate, Button, Tabs, Modal } from 'antd';
+import { Spin, Alert, Card, Col, Row, Rate, Button, Tabs, Modal } from 'antd';
 const { Meta } = Card;
-const { TabPane } = Tabs;
+const { Item } = Tabs;
 import { ShoppingCartOutlined, HeartOutlined, HeartFilled } from '@ant-design/icons';
 import styles from './detail.module.css';
 
@@ -31,55 +31,57 @@ const Detail = () => {
 
     return(
         <div className={styles.detailContainer}>
-            { isLoading&&       
-                <Spin tip="Cargando" size="large">
-                    <div className="content" />
-                </Spin> 
-            }
-            { isError || productDetail === undefined &&
-                <Alert message="Error" description="Por favor, intente de nuevo más tarde." type="error" showIcon className={styles}/>
-            }
-            <Card>
-                <Row>
-                    <Col span={16} className={styles.span}>
-                        <img alt={productDetail.name} src={productDetail.image} className={styles.image} />
-                    </Col>
-                    <Col span={8}>
-                        <div className={styles.productInfo}>
-                            {isFav ? <HeartOutlined size="large" className={styles.noLikedButton} onClick={handleFav}/>
-                            : <HeartFilled size="large" className={styles.likedButton} onClick={handleFav} />}
-                            <h1>{productDetail.name}</h1> 
-                            <h2>${productDetail.price}</h2> 
-                            <Rate value={productDetail.rating}/> 
-                            <p>{productDetail.category}</p> 
-                            <Meta description={<p>id: {productDetail.id}</p>}/> <br /> 
-                            <div className={styles.productButtons}>
-                                <Button type="default" block><ShoppingCartOutlined size="large"/></Button> 
-                                <Button type="primary" block className={styles.buttonComprar} onClick={showModal}>Comprar</Button>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col span={24}>
-                        <Card >
-                        <Tabs defaultActiveKey="1">
-                                <TabPane tab="Descripción" key="1">
-                                    <div style={{ maxHeight: '50%', overflow: 'auto', textAlign: 'center' }}>
-                                        <p>{productDetail.description}</p>
+            { isLoading ? <Spin tip="Cargando" className={styles.loading}><div className="content"/></Spin> 
+
+                : productDetail !== undefined ?
+                <div>
+                        <Card>
+                            <Row>
+                                <Col span={16} className={styles.span}>
+                                    <img alt={productDetail.name} src={productDetail.image} className={styles.image} />
+                                </Col>
+                                <Col span={8}>
+                                    <div className={styles.productInfo}>
+                                        {isFav ? <HeartOutlined size="large" className={styles.noLikedButton} onClick={handleFav}/>
+                                        : <HeartFilled size="large" className={styles.likedButton} onClick={handleFav} />}
+                                        <h1>{productDetail.name}</h1> 
+                                        <h2>${productDetail.price}</h2> 
+                                        <Rate value={productDetail.rating}/> 
+                                        <p>{productDetail.category}</p> 
+                                        <Meta description={<p>id: {productDetail.id}</p>}/> <br /> 
+                                        <div className={styles.productButtons}>
+                                            <Button type="default" block><ShoppingCartOutlined size="large"/></Button> 
+                                            <Button type="primary" block className={styles.buttonComprar} onClick={showModal}>Comprar</Button>
+                                        </div>
                                     </div>
-                                </TabPane>
-                                <TabPane tab="Comentarios" key="2">
-                                    <div style={{ maxHeight: '50%', overflow: 'auto', textAlign: 'center' }}>
-                                        <p>No hay comentarios disponibles</p>
-                                    </div>
-                                </TabPane>
-                            </Tabs>
+                                </Col>
+                                <Col span={24}>
+                                    <Card >
+                                    <Tabs defaultActiveKey="1">
+                                            <Item tab="Descripción" key="1">
+                                                <div style={{ maxHeight: '50%', overflow: 'auto', textAlign: 'center' }}>
+                                                    <p>{productDetail.description}</p>
+                                                </div>
+                                            </Item>
+                                            <Item tab="Comentarios" key="2">
+                                                <div style={{ maxHeight: '50%', overflow: 'auto', textAlign: 'center' }}>
+                                                    <p>No hay comentarios disponibles</p>
+                                                </div>
+                                            </Item>
+                                        </Tabs>
+                                    </Card>
+                                </Col>
+                            </Row>
                         </Card>
-                    </Col>
-                </Row>
-            </Card>
-                <Modal title="Confirmar compra" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
-                    <p>¿Estás seguro de que quieres comprar este producto?</p>
-                </Modal>
+                            <Modal title="Confirmar compra" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                                <p>¿Estás seguro de que quieres comprar este producto?</p>
+                            </Modal>
+                        </div>
+                        :
+                        <Alert message="Error" description="Por favor, intente de nuevo más tarde." type="error" showIcon className={styles}/>
+
+            }
+
         </div>
     );
 };

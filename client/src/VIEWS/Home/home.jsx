5d8f08dc0ef4274c3467220c97e14513aa662471
Javier.pjs <javier.sosa.1911@gmail.com>
2023-10-28 15:33:25 -0300

    icono whatsapp terminado

--- client/src/VIEWS/Home/home.jsx
+++ client/src/VIEWS/Home/home.jsx
@@ -3,15 +3,19 @@ import Carousel from '../../Components/carousel/Carousel';
 import Searchbar from '../../Components/searchBar/Searchbar';
 import styles from './home.module.css'
 import Filters from '../../Components/FIlters/Filters.jsx';
+import { WhatsAppOutlined } from '@ant-design/icons'
 
 const Home = () =>{
-
+    const whatsappLink = `https://wa.me/+5493816771213`;
     return(
         <div className ={styles.homeContainer}>
             <Carousel/>
             <Searchbar/>
             <Filters/>
             <Cards/>
+            <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+                <WhatsAppOutlined className={styles['whatsapp-icon']} />
+            </a>
 
         </div>
     );

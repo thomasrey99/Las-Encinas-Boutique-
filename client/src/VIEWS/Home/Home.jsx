e93aa686821a108165a0b4590f21a46d742eb802
Mariano Masondo <116052945+MarianoMasondo@users.noreply.github.com>
2023-10-28 14:20:35 -0300

    Mariano masondo (#77)
    * main
    
    * commit mariano
    
    * commit env
    
    * Paginado 1
    
    * env actualizado
    
    * paginado ok
    
    * agregué Mapa
    
    * Map con ubicaion
    
    * card
    
    * env
    
    * card
    
    * card changes
    
    * paginado Ant
    
    * card change

---
+++ client/src/VIEWS/Home/Home.jsx
@@ -0,0 +1,54 @@
+import { Pagination } from "antd";
+import Card from "../../Components/Card/Card.jsx";
+import IniciarMap from "../../Components/Maps/Maps.jsx";
+import Carousel from "../../Components/carousel/Carousel";
+import Searchbar from "../../Components/searchBar/Searchbar";
+import { setCurrentPage } from "../../libs/redux/features/productsSlice.js";
+import styles from "./home.module.css";
+import { useDispatch, useSelector } from "react-redux";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.items.allProducts);
+  const currentPage = useSelector((state) => state.items.currentPage);
+  const itemsPerPage = useSelector((state) => state.items.itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const productsToDisplay = products.slice(startIndex, endIndex);
+
+  const paginate = (pageNumber) => {
+    dispatch(setCurrentPage(pageNumber));
+  };
+
+  return (
+    <div className={styles.homeContainer}>
+      <Carousel />
+      <Searchbar />
+      <div className={styles.pagCont}>
+      <Pagination
+        current={currentPage}
+        pageSize={itemsPerPage}
+        total={products.length}
+        onChange={paginate}
+      />
+      </div>
+
+      <div className={styles.cardCont}>
+        {productsToDisplay?.map((product) => (
+          <Card
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+            raiting={product.raiting}
+          />
+        ))}
+      </div>
+      <IniciarMap />
+    </div>
+  );
+};
+
+export default Home;

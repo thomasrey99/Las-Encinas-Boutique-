177abcaaf86cd6e28f2c9c164725498884aa59be
Mariano Masondo <nanomason@gmail.com>
2023-10-28 13:44:21 -0300

    paginado Ant

--- client/src/Components/Pagination/Pagination.jsx
+++ client/src/Components/Pagination/Pagination.jsx
@@ -1,49 +1,45 @@
-import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 import styles from "./Pagination.module.css";
 
-const Pagination = ({ currentPage, productsPerPage, products, paginate}) => {
-    const totalPages = Math.ceil(products.length / productsPerPage);
+const Pagination = () => {
+  const { currentPage, itemsPerPage, totalItems, paginate } = useSelector(
+    (state) => state.pagination
+  );
 
-    const pages = [];
-    for(let i = 1; i <= totalPages; i++) {
-        pages.push(i);
-    }
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-    return (
-        <nav className={styles.pagCont}>
-            <div className={styles.pagButtons}>
-                <button
-                onClick={() => paginate(currentPage -1)}
-                // className={currentPage === 1 ? styles.disabled : ''}
-                disabled={currentPage === 1}
-                >
-                    &#8592;
-                </button>
-                {pages.map((page) => (
-                    <button
-                    key={page}
-                    onClick={() => paginate(page)}
-                    className={currentPage === page ? styles.active : ""}
-                    >
-                        {page}
-                    </button>
-                ))}
-                <button
-                onClick={() => paginate(currentPage + 1)}
-                disabled={currentPage === totalPages}
-                >
-                    &#8594;
-                </button>
-            </div>
-        </nav>
-    )
-}
-Pagination.propTypes ={
-    currentPage: PropTypes.number.isRequired,
-    productsPerPage: PropTypes.number.isRequired,
-    products: PropTypes.array.isRequired,
-    paginate: PropTypes.func.isRequired,
-    }
+  const pages = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
 
+  return (
+    <nav className={styles.pagCont}>
+      <div className={styles.pagButtons}>
+        <button
+          onClick={() => paginate(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          &#8592;
+        </button>
+        {pages.map((page) => (
+          <button
+            key={page}
+            onClick={() => paginate(page)}
+            className={currentPage === page ? styles.active : ""}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          onClick={() => paginate(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          &#8594;
+        </button>
+      </div>
+    </nav>
+  );
+};
 
 export default Pagination;
--- client/src/VIEWS/Home/Home.jsx
+++ client/src/VIEWS/Home/Home.jsx
@@ -1,67 +1,54 @@
-// // import { Cards } from '../../Components/cards/Cards.jsx';
-import Card from "../../Components/Card/Card";
-import Pagination from "../../Components/Pagination/Pagination";
+import { Pagination } from "antd";
+import Card from "../../Components/Card/Card.jsx";
+import IniciarMap from "../../Components/Maps/Maps.jsx";
 import Carousel from "../../Components/carousel/Carousel";
 import Searchbar from "../../Components/searchBar/Searchbar";
-import IniciarMap from "../../Components/Maps/Maps";
+import { setCurrentPage } from "../../libs/redux/features/productsSlice.js";
 import styles from "./home.module.css";
-import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
-  const [currentPage, setCurrentPage] = useState(1);
-  const productsPerPage = 4;
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.items.allProducts);
+  const currentPage = useSelector((state) => state.items.currentPage);
+  const itemsPerPage = useSelector((state) => state.items.itemsPerPage);
 
-  const products = [
-    { nombre: "Chocolate Blanco", precio: 2.99 },
-    { nombre: "Chocolate con Leche", precio: 3.49 },
-    { nombre: "Chocolate Amargo 70%", precio: 4.99 },
-    { nombre: "Chocolate de Avellanas", precio: 3.79 },
-    { nombre: "Chocolate de Caramelo", precio: 2.89 },
-    { nombre: "Chocolate de Fresa", precio: 2.99 },
-    { nombre: "Chocolate de Naranja", precio: 2.99 },
-    { nombre: "Chocolate de Menta", precio: 2.99 },
-    { nombre: "Chocolate con Almendras", precio: 3.29 },
-    { nombre: "Chocolate de Vainilla", precio: 2.99 },
-    { nombre: "Chocolate de Cereza", precio: 3.09 },
-    { nombre: "Chocolate de Coco", precio: 2.99 },
-    { nombre: "Chocolate de Frambuesa", precio: 3.19 },
-    { nombre: "Chocolate de Café", precio: 3.29 },
-    { nombre: "Chocolate de Plátano", precio: 2.99 },
-    { nombre: "Chocolate de Canela", precio: 2.99 },
-    { nombre: "Chocolate de Maracuyá", precio: 3.39 },
-    { nombre: "Chocolate de Pistachos", precio: 3.49 },
-    { nombre: "Chocolate de Mora", precio: 3.19 },
-    { nombre: "Chocolate de Melocotón", precio: 3.09 },
-  ];
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const productsToDisplay = products.slice(startIndex, endIndex);
 
   const paginate = (pageNumber) => {
-    setCurrentPage(pageNumber);
-    console.log("Página actual:", pageNumber);
+    dispatch(setCurrentPage(pageNumber));
   };
 
   return (
     <div className={styles.homeContainer}>
       <Carousel />
       <Searchbar />
+      <div className={styles.pagCont}>
       <Pagination
-        currentPage={currentPage}
-        productsPerPage={productsPerPage}
-        products={products}
-        paginate={paginate}
+        current={currentPage}
+        pageSize={itemsPerPage}
+        total={products.length}
+        onChange={paginate}
       />
-      <div>
-        {products.map((c) => (
-          <Card key={c.nombre} name={c.nombre} price={c.precio} />
-        ))}
-      </div>
-      <div className={styles.bannCont}>
-        <IniciarMap />
       </div>
 
-      {/* <Cards products={products} /> */}
+      <div className={styles.cardCont}>
+        {productsToDisplay?.map((product) => (
+          <Card
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+            raiting={product.raiting}
+          />
+        ))}
+      </div>
+      <IniciarMap />
     </div>
   );
 };
-
 
 export default Home;

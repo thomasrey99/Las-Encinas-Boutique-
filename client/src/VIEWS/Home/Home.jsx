7397a069c98f6b051be05bcb114d4fde6f52faa1
Javier.pjs <javier.sosa.1911@gmail.com>
2023-10-28 16:31:27 -0300

    home solucionado

---
+++ client/src/VIEWS/Home/Home.jsx
@@ -0,0 +1,60 @@
+import { Pagination } from "antd";
+import Card from "../../Components/Card/Card.jsx";
+import IniciarMap from "../../Components/Maps/Maps.jsx";
+import Carousel from "../../Components/carousel/Carousel.jsx";
+import Searchbar from "../../Components/searchBar/Searchbar.jsx";
+import { setCurrentPage } from "../../libs/redux/features/productsSlice.js";
+import styles from "./home.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { WhatsAppOutlined } from '@ant-design/icons'
+
+const Home = () => {
+  const whatsappLink = `https://wa.me/+5493816771213`;  
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.items.allProducts);
+  const currentPage = useSelector((state) => state.items.currentPage);
+  const itemsPerPage = useSelector((state) => state.items.itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const productsToDisplay = products.slice(startIndex, endIndex);
+
+  const paginate = (pageNumber) => {
+    dispatch(setCurrentPage(pageNumber));
+  };
+
+  return (
+    <div className={styles.homeContainer}>
+      <Carousel />
+      <Searchbar />
+      <div className={styles.pagCont}>
+      <Pagination
+        current={currentPage}
+        pageSize={itemsPerPage}
+        total={products.length}
+        onChange={paginate}
+      />
+      </div>
+
+      <div className={styles.cardCont}>
+        {productsToDisplay?.map((product) => (
+          <Card
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.image}
+            raiting={product.raiting}
+          />
+        ))}
+      </div>
+      <IniciarMap />
+      <hr />
+      <a href={whatsappLink} target="_blank" rel="noopener noreferrer">
+        <WhatsAppOutlined className={styles['whatsapp-icon']} />
+      </a>
+    </div>
+  );
+};
+
+export default Home;

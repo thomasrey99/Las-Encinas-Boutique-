63b00bd73445258cbc43e59ff56888a2d1a97d73
NahimMora <nahimsalta@gmail.com>
2023-10-29 18:16:31 -0300

    Pre pull develop

--- client/src/VIEWS/FormProduct/FormProducts.jsx
+++ client/src/VIEWS/FormProduct/FormProducts.jsx
@@ -1,17 +1,12 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
-import { Button, Form, Rate, Select, Upload, Input, InputNumber } from 'antd';
-
-import { useCreateProductMutation } from '../../libs/redux/services/productsApi';
+import { Button, Form, Rate, Select, Input, InputNumber } from 'antd';
 
 const { Option } = Select;
 const { TextArea } = Input;
 
 const FormProducts = () => {
-
-  const products=useSelector((state)=>state.items.allProducts)
-
+  const products = useSelector((state) => state.items.allProducts);
   const dispatch = useDispatch();
 
   const [state, setState] = useState({
@@ -22,8 +17,6 @@ const FormProducts = () => {
     rating: 0,
     category: [],
   });
-
-  const [data] = useCreateProductMutation(state)
 
   const resetState = () => {
     setState({
@@ -37,7 +30,6 @@ const FormProducts = () => {
   };
 
   const handleChange = (name, value) => {
-    console.log(state);
     setState({
       ...state,
       [name]: value,
@@ -50,78 +42,132 @@ const FormProducts = () => {
     // resetState();
   };
 
-  const categorys = []
-  for (let i = 0; i < products.length; i++) {
-    categorys.push(i.category);
-    console.log(i.category);
-  }
-
-  console.log(categorys);
-
   return (
     <>
       <div>FormProducts</div>
-      <form>
-        <Form.Item label="Nombre">
-          <Input
-            name="name"
-            value={state.name}
-            onChange={(e) => handleChange('name', e.target.value)}
-          />
-        </Form.Item>
-
-        <Form.Item label="Descripcion">
-          <TextArea
-            rows={4}
-            name='description'
-            value={state.description}
-            onChange={(e) => handleChange('description', e.target.value)}
-          />
-        </Form.Item>
-
-        <Form.Item label="Precio">
-          <InputNumber
-            min={1}
-            name="price"
-            value={state.price}
-            onChange={(value) => handleChange('price', value)}
-          />
-        </Form.Item>
-
-        <Form.Item name='rating' label="Rate">
-          <Rate
-            value={state.rating}
-            onChange={(value) => handleChange('rating', value)}
-          />
-        </Form.Item>
-
-        <Form.Item
-          name="category"
-          label="Categorias"
-          rules={[
-            {
-              required: true,
-              message: 'Elije al menos una categoría',
-              type: 'array',
-            },
-          ]}
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <form
+          style={{
+            backgroundColor: '#8B4513', // Color de fondo marrón
+            padding: '20px', // Agregamos espacio alrededor del formulario
+            display: 'flex',
+            flexDirection: 'row', // Alinear elementos en fila
+            flexWrap: 'wrap', // Permitir que los elementos se envuelvan si no caben en una línea
+            gap: '20px', // Espacio entre elementos
+          }}
         >
-          <Select
-            mode="multiple"
-            placeholder="Elije una categoría"
-            value={state.category}
-            onChange={(value) => handleChange('category', value)}
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flex: '1', // Ocupa todo el espacio disponible en la fila
+              
+            }}
           >
-            {products?.map((c) => (
-              <Option value={c.name} key={c.id}>
-                {categorys}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
+            <Form.Item label="Nombre">
+              <Input
+                name="name"
+                value={state.name}
+                onChange={(e) => handleChange('name', e.target.value)}
+                style={{
+                  backgroundColor: '#D2B48C',
+                  width: '70%',
+                }}
+              />
+            </Form.Item>
 
-        <button onClick={handleSubmit}>Crear producto</button>
-      </form>
+            <Form.Item label="Descripcion">
+              <TextArea
+                rows={4}
+                name="description"
+                value={state.description}
+                onChange={(e) => handleChange('description', e.target.value)}
+                style={{
+                  backgroundColor: '#D2B48C',
+                  width: '100%',
+                }}
+              />
+            </Form.Item>
+          </div>
+
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              flex: '1', // Ocupa todo el espacio disponible en la fila
+            }}
+          >
+            <Form.Item label="Precio" style={{ minWidth: '50%' }}>
+              <InputNumber
+                min={1}
+                name="price"
+                value={state.price}
+                onChange={(value) => handleChange('price', value)}
+                style={{
+                  backgroundColor: '#D2B48C',
+                  width: '100%',
+                }}
+              />
+            </Form.Item>
+
+            <Form.Item name="rating" label="Rate" style={{ minWidth: '50%' }}>
+              <Rate
+                value={state.rating}
+                onChange={(value) => handleChange('rating', value)}
+                style={{ width: '100%' }}
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="category"
+              label="Categorias" 
+              style={{ minWidth: '50%' }}
+              rules={[
+                {
+                  required: true,
+                  message: 'Elije al menos una categoría',
+                  type: 'array',
+                },
+              ]}
+            >
+              <Select
+                mode="multiple"
+                placeholder="Elije una categoría"
+                value={state.category}
+                onChange={(value) => handleChange('category', value)}
+                style={{
+                  backgroundColor: '#D2B48C',
+                  width: '100%',
+                }}
+              >
+                {products.map((product) => (
+                  <Option value={product.name} key={product.id}>
+                    {product.category}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </div>
+          <button
+            onClick={handleSubmit}
+            style={{
+              backgroundColor: '#D2B48C',
+              color: 'black',
+              border: 'none',
+              padding: '10px 20px',
+              cursor: 'pointer',
+              width: '100%',
+            }}
+          >
+            Crear producto
+          </button>
+        </form>
+      </div>
     </>
   );
 };

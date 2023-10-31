20e473cdca12f32fa003a089affa7dc254bbb90e
Javier.pjs <javier.sosa.1911@gmail.com>
2023-10-31 18:27:43 -0300

    actualizar

--- client/src/VIEWS/Forms/FormsUser/user.jsx
+++ client/src/VIEWS/Forms/FormsUser/user.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { useState } from 'react';
 import Validates from './validates';
 import { useCreateUsersMutation } from '../../../libs/redux/services/usersApi';
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, message } from 'antd';
+import style from './user.module.css';
+
 
 
 const FormUser = () => {
+    const { Item } = Form
     const { Password } = Input;
     const userForm = useSelector(state => state.user)
     const [mutate] = useCreateUsersMutation();
@@ -71,37 +75,65 @@ const FormUser = () => {
     }
 console.log(form);
     return (
-        <div>
-            <h1>Registro de Usuario</h1>
-            <form onSubmit={handlerSubmit}>
-                <label htmlFor="name">Nombre: </label>
-                <Input type="text" name= 'name'  placeholder='Ingresar nombre...' onChange={handlerCange} />
-                {errors.name !== '' ? <span>{errors.name}</span> : ''}
-                <hr />
-                <label htmlFor="lastName">Apellido: </label>
-                <Input type="text" name= 'lastName'  placeholder='Ingresar apellido...' onChange={handlerCange} />
-                {errors.lastName !== '' ? <span>{errors.lastName}</span> : ''}
-                <hr />
-                <label htmlFor="address">Dirección: </label>
-                <Input type="text" name= 'address'  placeholder='Ingresar dirección...' onChange={handlerCange} />
-                {errors.address !== '' ? <span>{errors.address}</span> : ''}
-                <hr />
-                <label htmlFor="email">E-Mail: </label>
-                <Input type="text" name= 'email'  placeholder='Escribe tu e-mail...' onChange={handlerCange} />
-                {errors.email !== '' ? <span>{errors.email}</span> : ''}
-                <hr />
-                {/* <label htmlFor="phone">Teléfono: </label>
-                <input type="text" name= 'phone'  placeholder='Dejanos tu contacto...' onChange={handlerCange} />
-                {errors.phone !== '' ? <span>{errors.phone}</span> : ''}
-                <hr /> */}
-                <label htmlFor="password">Contraseña: </label>
-                <Password type="text" name= 'password' placeholder='Debe ser secreta...' onChange={handlerCange} />
-                {errors.password !== '' ? <span>{errors.password}</span> : ''}
-                <hr />
+        <div className={style.div}>
+            <div className={style.containerPrincipal}>
+                <div className={style.containerSegundario}>
+                    <Form name= 'form' onSubmit={handlerSubmit}>
+                        <Item label='Nombre:'
+                        name='name'
+                        rules={[{
+                            required: true,
+                            message: 'Ingresa tu nombre'
+                        }]}>
+                            <Input name='name' onChange={handlerCange}/>
+                            {errors.name !== '' ? <span>{errors.name}</span> : ''}
+                        </Item>
+ 
+                        <Item label='Apellido:'
+                        name='lastName'
+                        rules={[{
+                            required: true,
+                            message: 'Ingresa tu apellido'
+                      }]}>
+                          <Input name='lastName' onChange={handlerCange}/>
+                          {errors.lastName !== '' ? <span>{errors.lastName}</span> : ''}
+                     </Item>
 
-                <button type='submit' disabled={!isFormValid} >REGISTRAR</button>
+                      <Item label='E-mail:'
+                      name='email'
+                      rules={[{
+                         required: true,
+                         message: 'Ingresa tu e-mail'
+                     }]}>
+                         <Input name='email' onChange={handlerCange}/>
+                         {errors.email !== '' ? <span>{errors.email}</span> : ''}
+                      </Item>
 
-            </form>
+                     <Item label='Contraseña'
+                        name='password'
+                     rules={[{
+                            required: true,
+                            message: 'Ingresa una contraseña'
+                     }]}>
+                          <Password name='password' onChange={handlerCange}/>
+                          {errors.password !== '' ? <span>{errors.password}</span> : ''}
+                     </Item>
+
+                     <Item label='Dirección:'
+                      name='address'
+                      rules={[{
+                            required: true,
+                            message: 'Ingresa tu dirección'
+                     }]}>
+                            <Input name='address' onChange={handlerCange}/>
+                           {errors.address !== '' ? <span>{errors.address}</span> : ''}
+                      </Item>
+                    
+                     <Button type='primary' htmlType='submit'>Registrar</Button>
+
+                    </Form>
+                </div>
+            </div>
         </div>
     )
 };

8bcd66ddc7a72b6504863773ee04ffa057ac16bb
thomasrey99 <thomas_rey1999@outlook.com>
2023-10-31 22:01:22 -0300

    cree slice de categorias

---
+++ client/src/libs/redux/features/typesSlice.js
@@ -0,0 +1,19 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState={
+    allCategories:[]
+}
+
+export const categoriesSlice=createSlice({
+    name:"categories",
+    initialState,
+    reducers:{
+        addCategories (state, {payload}){
+            state.allCategories=[
+                ...payload
+            ]
+        }
+    }
+})
+
+export const {addCategories}=categoriesSlice.actions

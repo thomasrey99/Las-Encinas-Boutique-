221ce67a4ad61f362ba6d4520eaa633d49787707
Mariano Masondo <nanomason@gmail.com>
2023-11-07 15:50:55 -0300

    Landing Start

--- client/src/VIEWS/Landing/Landing.jsx
+++ client/src/VIEWS/Landing/Landing.jsx
@@ -1,9 +1,29 @@
-import styles from "./Landing.module.css";
+import { useState, useEffect } from 'react';
+import style from './Landing.module.css';
+import logo from '../../assets/Las_encinas_Logo.png';
+import Home from '../Home/Home';
+
 
 const Landing = () => {
-  return (
-    <div>Landing</div>
-  )
-}
+  const [showLanding, setShowLanding] = useState(true);
 
-export default Landing
+  useEffect(() => {    
+    const timer = setTimeout(() => {
+      setShowLanding(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      {showLanding ? (
+        <img src={logo} className={style.img} alt="Logo" />
+      ) : (
+        <Home />
+      )}
+    </div>
+  );
+};
+
+export default Landing;

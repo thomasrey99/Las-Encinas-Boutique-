ee8802b82db25bf31fbdfa85df6ed02177a6239a
Mariano Masondo <nanomason@gmail.com>
2023-11-09 00:49:41 -0300

    About scrolling AOS

--- client/src/VIEWS/AboutUs/aboutUs.jsx
+++ client/src/VIEWS/AboutUs/aboutUs.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+
 import './aboutUs.css'
 import { IoLogoLinkedin, IoLogoGithub } from 'react-icons/io5'
 import {MdOutlineMailOutline} from 'react-icons/md'
 import { GithubOutlined, LinkedinOutlined, MailOutlined } from '@ant-design/icons'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // Importa el archivo CSS de AOS
+import { useEffect } from 'react';
+
 
 const coders = [
   {
     id: 1,
     fullName: "Cristian Puyo",
+    description:"",
     linkAvatar: "/public/cris.jpeg",
     linkedin: " https://www.linkedin.com/in/christian-dustin-puyo-torres-b08082253/",
     github: "#",
@@ -15,15 +20,17 @@ const coders = [
   },
   {
     id: 2,
-    fullName: "Mariano Masondo",
-    linkAvatar: "/public/Mariano.jpg",
-    linkedin: "https://www.linkedin.com/in/mariano-masondo-7026b0254",
-    github: "https://github.com/MarianoMasondo",
-    email: "nanomason@gmail.com"
+    fullName: "Liliana Rosada",
+    description:"",
+    linkAvatar: "/public/lil.jpeg",
+    linkedin: "https://www.linkedin.com/in/liliana-rosada-46114516a",
+    github: "https://github.com/LILYCERON",
+    email: "lilyceron00@gmail.com"
   },
   {
     id: 3,
     fullName: "Javier Sosa",
+    description:"",
     linkAvatar: "/public/CP.webp",
     linkedin: "https://www.linkedin.com/in/pablo-javier-sosa-179a31195/",
     github: "https://github.com/JavierSosaAdm",
@@ -32,6 +39,7 @@ const coders = [
   {
     id: 4,
     fullName: "Nahim Mora",
+    description:"",
     linkAvatar: "/public/Nah.webp",
     linkedin: "in/fernando-nahim-mora-developer",
     github: "https://github.com/NahimMora",
@@ -40,6 +48,7 @@ const coders = [
   {
     id: 5,
     fullName: "Mateo Leon",
+    description:"",
     linkAvatar: "/public/CP.webp",
     linkedin: " https://www.linkedin.com/in/mateo-le%C3%B3n-097b57268?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", 
     github: "https://github.com/MateoLeon505",
@@ -48,6 +57,7 @@ const coders = [
   {
     id: 6,
     fullName: "Thomas Rey",
+    description:"",
     linkAvatar: "/public/CP.webp",
     linkedin: "#",
     github: "https://github.com/thomasrey99",
@@ -55,32 +65,34 @@ const coders = [
   },
   {
     id: 7,
-    fullName: "Liliana Rosada",
-    linkAvatar: "/public/lil.jpeg",
-    linkedin: "https://www.linkedin.com/in/liliana-rosada-46114516a",
-    github: "https://github.com/LILYCERON",
-    email: "lilyceron00@gmail.com"
+    fullName: "Mariano Masondo",
+    description: "Desarrollador Full Stack apasionado por la tecnología en busca constante de soluciones en base a la experiencia, creatividad y disposición para aportar a tu equipo.",
+    linkAvatar: "/public/Mariano.jpg",
+    linkedin: "https://www.linkedin.com/in/mariano-masondo-7026b0254",
+    github: "https://github.com/MarianoMasondo",
+    email: "nanomason@gmail.com"
+    
   }
 ]
 
 const aboutUs = () => {
+  useEffect(() => {
+    AOS.init(); 
+  }, []);
 
   return (
     <div className='container'>
       <h3 style={{fontFamily: 'var(--primary-font)'}}>Un equipo excelente</h3>
       <div>
-        <p style={{fontFamily: 'var(--primary-font)', textAlign: 'center', margin: '0px 150px'}}>Nace de la inspiración, pasión y práctica del conocimiento para el
-          servicio de cada emprendedor.
-          Capacitados y comprometidos con la realización de proyectos, un equipo conformado por 8
-          personas con grandes habilidades comunicativas, laborales y sociales.</p>
+        <p style={{fontFamily: 'var(--primary-font)', textAlign: 'center', margin: '0px 150px'}}>Un equipo excepcional de 7 desarrolladores fullstack se forja a través de la inspiración, pasión y un profundo dominio del conocimiento, destinado a servir a cada emprendedor. Estamos dedicados y altamente comprometidos con la ejecución de proyectos, y nuestro equipo está conformado por profesionales con sólidas habilidades de comunicación, experiencia laboral y una destacada aptitud social.</p>
       </div>
       <div className='cards__container'>
         {coders.map((coder) => {
           return (
-            <div key={coder.id} className='card'>
+            <div key={coder.id} className='card' data-aos="zoom-in" data-aos-delay="500" data-aos-duration="1000">
               <img className='card__image' src={`${coder.linkAvatar}`} width="200" height="200" />
               <h5 className='card__text'>{coder.fullName}</h5>
-              <p style={{fontFamily: 'var(--primary-font)', textAlign: 'center'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, consequatur!</p>
+              <p style={{fontFamily: 'var(--primary-font)', textAlign: 'center'}}>{coder.description}</p>
               <div className='card__links'>
                 <a href={`${coder.github}`}><GithubOutlined /></a>
                 <a href={`mailto:${coder.email}`}><MailOutlined /></a>

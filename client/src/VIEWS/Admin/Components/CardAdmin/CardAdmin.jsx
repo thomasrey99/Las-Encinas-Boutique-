13d05e2c3c27ce402766739166e494ee8ec70ffc
Liliana <lilyceron00@gmail.com>
2023-11-10 23:06:30 -0500

    Add logical delete

--- client/src/VIEWS/Admin/Components/CardAdmin/CardAdmin.jsx
+++ client/src/VIEWS/Admin/Components/CardAdmin/CardAdmin.jsx
@@ -1,4 +1,4 @@
-import { EditOutlined, DeleteOutlined} from '@ant-design/icons';
+import { EditOutlined, DeleteOutlined, CheckCircleOutlined } from '@ant-design/icons';
 import { Card } from 'antd';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux/es/hooks/useSelector';
@@ -6,12 +6,27 @@ import { useSoftDeleteMutation } from '../../../../libs/redux/services/productsA
 
 const { Meta } = Card;
 
+const ActivateButton = ({ handleClick, isDelete }) => {
+  const buttonn = isDelete && <span onClick={handleClick}><CheckCircleOutlined key='activate' /></span>
+  return buttonn
+}
+
+const DeleteButton = ({ handleClick, isDelete }) => {
+  const buttonn = !isDelete && <span onClick={handleClick}><DeleteOutlined key='delete' /></span>
+  return buttonn
+}
+
 const CardAdmin = ({ image, name, description, id_product, is_Delete }) => {
 
   const [softDelete, { data, isLoading, isError, error, }] = useSoftDeleteMutation()
   const descriptionEdit = description.slice(0, 35)
+  const products = useSelector((state) => state.items.allProducts)
 
-  const handleDelete=()=> {
+  const handleEditClick = () => {
+    setIsEditing(true)
+  }
+
+  const handleDelete = () => {
     const body = {
       id_product: id_product,
       is_Delete: true
@@ -19,13 +34,23 @@ const CardAdmin = ({ image, name, description, id_product, is_Delete }) => {
     softDelete(body)
   }
 
+  const handleActivate = () => {
+    const body = {
+      id_product: id_product,
+      is_Delete: false
+    }
+    softDelete(body)
+  }
+
   return (
     <div>
-        <Card
+      <Card
         style={{
           margin: "5% 0 0 0",
           width: "18em",
-          border: is_Delete ? "solid 2px red" : "transparent"
+          backgroundColor: is_Delete ? "#ffe6e6" : "white", // Fondo rojo claro si es eliminado
+          color: is_Delete ? "darkred" : "black", // Texto oscuro si es eliminado
+          boxShadow: is_Delete ? "0 0 10px rgba(255, 0, 0, 0.5)" : "none", // Sombra roja si es eliminado
         }}
         cover={
           <img
@@ -37,21 +62,22 @@ const CardAdmin = ({ image, name, description, id_product, is_Delete }) => {
               position: "static",
               top: "0",
               left: "0"
-              }}
+            }}
           />
         }
         actions={[
-          <button onClick={handleDelete}><DeleteOutlined /></button>,
-          <NavLink to={`/editProductAdmin/${id_product}`}><EditOutlined key="edit" /></NavLink >
+          <ActivateButton handleClick={handleActivate} isDelete={is_Delete} />,
+          <span onClick={handleEditClick}><EditOutlined key="edit" /></span>,
+          <DeleteButton handleClick={handleDelete} isDelete={is_Delete} />
         ]}
       >
-        <Meta 
-          style={{minHeight: "6em"}}
+        <Meta
+          style={{ minHeight: "6em" }}
           title={name}
           description={descriptionEdit + "..."}
         />
       </Card>
-      </div>
+    </div>
   )
 }
 

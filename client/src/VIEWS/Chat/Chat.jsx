e9b576d1f558f0308c8f04a2638997e72a94d8e7
ChristianPuyo <christianpuyotorres@gmail.com>
2023-11-12 09:58:03 -0500

    Avance del chat in live

--- client/src/VIEWS/Chat/Chat.jsx
+++ client/src/VIEWS/Chat/Chat.jsx
@@ -1,96 +1,93 @@
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-import io from 'socket.io-client'
-const socket = io('http://localhost:3001') //para el chat en vivo
+import React, { useEffect, useState, useRef } from "react";
+import { useSelector } from "react-redux";
+import io from 'socket.io-client';
+import styles from './Chat.module.css'; // Asegúrate de que la ruta sea correcta
 
-// const Chat = ()=>{
-//     const currentUser = useSelector(state => state.user.userLog)
-//     const [message, setMessage] = useState('');
-//     const [messages, setMessages] =useState([])
-
-//     const handleSubmit = (e)=>{
-//         e.preventDefault();
-//         setMessages([...messages, message])
-//         socket.emit('message', message)
-//     }
-
-//     useEffect(()=>{
-//         socket.on('message',receiveMessage)
-//         return ()=>{
-//             socket.off('message',receiveMessage)
-//         }
-//     },[])
-
-//     const receiveMessage = (message) => setMessages((state)=>[...state, message])
-//     let user=''
-//     currentUser && (user = currentUser.name.toString())
-//     return(<div>
-//         <form onSubmit={handleSubmit}>
-//             <input 
-//             type="text" 
-//             placeholder='Type your message here'
-//             onChange={(e) =>setMessage(e.target.value)}
-//             />
-//             <button type='submit'>Send</button>
-//         </form>
-
-//         <ul>
-//             {messages.map((msg, index)=>(
-//                 <li key={index}>{user}:{msg}</li>
-//             ))}
-//         </ul>
-//     </div>)
-
-// }
-
-// export default Chat;
-
-// ...
+const socket = io('http://localhost:3001'); // para el chat en vivo
 
 const Chat = () => {
-    const currentUser = useSelector(state => state.user.userLog);
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      // Guardar el mensaje junto con el nombre del usuario
-      setMessages([...messages, { user: currentUser.name.toString(), text: message }]);
-      socket.emit('message', { user: currentUser.name.toString(), text: message });
-      setMessage(''); // Limpiar el campo de entrada después de enviar el mensaje
-    };
-  
-    useEffect(() => {
-      socket.on('message', receiveMessage);
-      return () => {
-        socket.off('message', receiveMessage);
-      };
-    }, []);
-  
-    const receiveMessage = (message) => setMessages((state) => [...state, message]);
-  
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Type your message here"
-            onChange={(e) => setMessage(e.target.value)}
-            value={message} // Agregar el valor para controlar el componente de entrada
-          />
-          <button type="submit">Send</button>
-        </form>
-  
-        <ul>
-          {messages.map((msg, index) => (
-            <li key={index}>
-              {msg.user}: {msg.text}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
+  const currentUser = useSelector(state => state.user.userLog);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const messageListRef = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMessage = { userName: currentUser.name.toString(), text: message };
+
+    // Emitir el mensaje al servidor de Socket.IO
+    socket.emit('message', newMessage);
+
+    // Enviar el mensaje a la ruta de tu servidor que espera mensajes
+    fetch('http://localhost:3001/messages', {
+      method: 'POST', // o el método que uses en tu servidor
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newMessage),
+    });
+
+    // Agregar el mensaje al estado local
+    setMessages((state) => [...state, newMessage]);
+
+    setMessage('');
   };
-  
-  export default Chat;
-  
+
+  useEffect(() => {
+    // Obtener mensajes al montar el componente
+    fetch('http://localhost:3001/messages')
+      .then(response => response.json())
+      .then(data => setMessages(data))
+      .catch(error => console.error('Error al obtener mensajes:', error));
+  }, []);
+
+  useEffect(() => {
+    if (messageListRef.current) {
+      // Scroll hacia abajo después de que los mensajes se hayan cargado
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }
+  }, [messages]);
+
+  const receiveMessage = (message) => {
+    // Agregar el mensaje al estado, ya sea del usuario actual o de otros usuarios
+    setMessages((state) => [...state, message]);
+  };
+
+  useEffect(() => {
+    socket.on('message', receiveMessage);
+    return () => {
+      socket.off('message', receiveMessage);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2 className={styles.titleChat}>Sala de Chat en Vivo</h2>
+<div className={styles.container}>
+      <ul ref={messageListRef} className={styles.messageList}>
+        {messages.map((msg, index) => (
+          <li key={index} className={styles.messageItem}>
+            <span>{msg.userName}:</span> {msg.text}
+          </li>
+        ))}
+      </ul>
+
+      <form onSubmit={handleSubmit} className={styles.messageInputContainer}>
+        <input
+          type="text"
+          placeholder="Escribe tu mensaje aquí"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          className={styles.messageInput}
+        />
+        <button type="submit" className={styles.sendButton}>Enviar</button>
+      </form>
+    </div>
+    </div>
+    
+  );
+};
+
+export default Chat; 
+
+
--- server/src/routes/mainRouter.js
+++ server/src/routes/mainRouter.js
@@ -7,6 +7,7 @@ const cartRoutes=require("./Cart")
 const favoritesRouter=require("./favorites")
 const reviewsRouter=require("./reviews");
 const paymentsRouter = require("./payment");
+const messagesRouter = require("./messages");
 
 const mainRouter = Router()
 
@@ -17,7 +18,8 @@ mainRouter.use("/types", typesRouter)
 mainRouter.use("/cart", cartRoutes)
 mainRouter.use("/favorites", favoritesRouter);
 mainRouter.use("/reviews", reviewsRouter);
-mainRouter.use("/payments", paymentsRouter)
+mainRouter.use("/payments", paymentsRouter);
+mainRouter.use("/messages", messagesRouter);
 
 module.exports = mainRouter;
 

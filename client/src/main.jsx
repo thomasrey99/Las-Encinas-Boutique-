7023b021d835b98b8c6ab35ba902898f75a110f4
mateoleont <mateolt505@gmail.com>
2023-11-14 15:45:13 -0500

    diseño antde

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,23 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './libs/redux/store.js';
+
+import { ConfigProvider } from 'antd';
+
+const config = {
+  theme: {
+    token: {
+      colorPrimary: '#eac696',
+      colorInfo: '#eac696',
+      fontSize: 15,
+      colorError: '#65451f',
+      colorWarning: '#65451f',
+      colorLink: '#c8ae7d',
+      colorSuccess: '#765827',
+    },
+  },
+};
+
 import { I18nextProvider } from "react-i18next";
 import i18next from "i18next";
 
@@ -30,7 +47,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter> 
       <React.StrictMode>
         <I18nextProvider i18n={i18next}>
-        <App />
+          <ConfigProvider {...config}>
+            <App />
+          </ConfigProvider>
         </I18nextProvider>
       </React.StrictMode>
     </BrowserRouter>
---
+++ client/src/theme.jsx
@@ -0,0 +1,12 @@
+
+export const theme = {
+    token: {
+      colorPrimary: '#eac696',
+      colorInfo: '#eac696',
+      fontSize: 15,
+      colorError: '#65451f',
+      colorWarning: '#65451f',
+      colorLink: '#c8ae7d',
+      colorSuccess: '#765827',
+    },
+  };

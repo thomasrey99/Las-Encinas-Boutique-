2f2a476089489f7bd3ee194f19a7dd08a8648c90
Mariano Masondo <nanomason@gmail.com>
2023-11-15 12:31:09 -0300

    banner translate

--- client/src/Components/infoProducts/banner.jsx
+++ client/src/Components/infoProducts/banner.jsx
@@ -5,10 +5,13 @@ import huevos from "../../../public/huevos.png"
 import pedido from "../../../public/pedido.png"
 import Aos from "aos"
 import "aos/dist/aos.css"
+import { useTranslation } from "react-i18next";
 
 import {useEffect} from "react"
 
 const Banner = () => {
+
+    const { t } = useTranslation("global");
 
     useEffect(()=>{
         Aos.init()
@@ -16,7 +19,7 @@ const Banner = () => {
 
   return (
     <section className={style.bannerCont}>
-        <h2 className={style.bannerTitle}>Disfrute de las mejores delicias de la region</h2>
+        <h2 className={style.bannerTitle}>{t("banner.title")}</h2>
         <div className={style.bannerItemsCont}>
 
             {/*primer banner*/}
@@ -26,8 +29,8 @@ const Banner = () => {
                     <img src={pedido}/>
                 </div>
                 <div className={style.textBanner}>
-                    <h3>Chocolates a tu Gusto: ¡Crea, Saborea, Disfruta!</h3>
-                    <p>Haz tus momentos dulces aún más especiales! Descubre nuestra gama de chocolates artesanales y crea tu propio paraíso de sabor con pedidos personalizados. ¡Convierte tus ideas en exquisitas realidades chocolatosas y haz tu pedido hoy mismo!</p>
+                    <h3>{t("banner.titledescription1")}</h3>
+                    <p>{t("banner.description1")}</p>
                 </div>
             </div>
 
@@ -38,8 +41,8 @@ const Banner = () => {
                     <img src={combo}/>
                 </div>
                 <div className={style.textBanner}>
-                    <h3>Combos Chocolatosos: ¡Encuentra Tu Dulce Equilibrio!</h3>
-                    <p>¡Descubre la perfección en cada combinación! Nuestras cajoneras de chocolates artesanales te ofrecen una experiencia única. Deléitate con nuestra selección cuidadosamente elaborada, ¡donde cada mordisco es una explosión de sabor! Encuentra tu combo ideal y sumérgete en un mundo de dulces sensaciones.</p>
+                    <h3>{t("banner.titledescription2")}</h3>
+                    <p>{t("banner.description2")}</p>
                 </div>
             </div>
 
@@ -50,8 +53,8 @@ const Banner = () => {
                     <img src={bombones}/>
                 </div>
                 <div className={style.textBanner}>
-                    <h3>Delicias Tentadoras: Bombones y Barras Artesanales para el Placer Absoluto</h3>
-                    <p>¡Dulces placeres en cada bocado! Explora nuestra variedad de bombones y barras de chocolate artesanales. Desde la indulgencia de nuestros exquisitos bombones hasta la satisfacción crujiente de nuestras barras, cada producto está creado para deleitar tus sentidos. Sumérgete en un mundo de tentaciones irresistibles y déjate seducir por la calidad de nuestros chocolates.</p>
+                    <h3>{t("banner.titledescription3")}</h3>
+                    <p>{t("banner.description3")}</p>
                 </div>
             </div>
 
@@ -62,8 +65,8 @@ const Banner = () => {
                     <img src={huevos}/>
                 </div>
                 <div className={style.textBanner}>
-                    <h3>¡Experimenta la Magia del Chocolate en Cada Bocado! Haz tu Pedido y Endúlzate con Nuestros Deliciosos Huevos Artesanales.</h3>
-                    <p>¿Qué estás esperando para deleitar tu paladar con nuestros exquisitos huevos de chocolate? Haz que cada momento sea más dulce y especial con nuestros productos artesanales. ¡Haz tu pedido ahora y deja que la magia del chocolate conquiste tu día!</p>
+                    <h3>{t("banner.titledescription4")}</h3>
+                    <p>{t("banner.description4")}</p>
                 </div>
             </div>
 

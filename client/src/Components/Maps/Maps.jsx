7b3a7ff87a33d35eccdcdc90446560ca334da702
Mariano Masondo <nanomason@gmail.com>
2023-11-15 16:07:36 -0300

    sin cambios importantes

--- client/src/Components/Maps/Maps.jsx
+++ client/src/Components/Maps/Maps.jsx
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import style from "./Maps.module.css"
+import { useTranslation } from "react-i18next";
+
 function IniciarMap() {
+
+  const { t } = useTranslation("global");
+
   useEffect(() => {
     const coord = { lat: -26.830462224358953, lng: -65.20385131873294 };
     const map = new window.google.maps.Map(document.getElementById("map"), {
@@ -16,7 +21,7 @@ function IniciarMap() {
 
   return (
     <div className={style.mapCont}>
-      <h2 className={style.ubicationTitle}>¿Donde nos encontramos?</h2>
+      <h2 className={style.ubicationTitle}>{t("map.Title")}</h2>
       <div id="map"className={style.map}></div>
     </div>
   )
